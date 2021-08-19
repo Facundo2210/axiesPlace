105325d03407dff7c164b665a8273326654c0943
Facundo2210 <74992465+Facundo2210@users.noreply.github.com>
2021-08-19 11:43:08 -0300

    plant

--- src/pages/RecentlyListed/index.js
+++ src/pages/RecentlyListed/index.js
@@ -7,13 +7,19 @@ import Filters from '../../components/Filters';
 import Spinner from '../../components/Spinner';
 import CoinsPrice from '../../components/CoinsPrice';
 import {
-	comboRonin,
+	/* 	comboRonin,
 	fishKoi,
 	fishKoiBreed0,
 	fishnimo,
+	fishAnemone,
+	birdDamage,
+	birdDamagePigeon,
+	comboDualBlade,
+	terminator, */
 	lessThan,
 	plantStandard,
-	fishAnemone,
+	plantStandardBreed0,
+	plantStandardBug,
 } from '../../utils/filterVars';
 
 const RecentlyListed = ({getEthPrice, getAxieLatest, filters}) => {
@@ -29,22 +35,34 @@ const RecentlyListed = ({getEthPrice, getAxieLatest, filters}) => {
 		setTimer(setInterval(updateAxies, 50));
 	};
 	//Axies menores a 250 usds
-	const _50usd = lessThan(axies, 50);
-	//const _250usd = lessThan(axies, 250);
+	const _250usd = lessThan(axies, 250);
 	//Filtrados
-	const roninCombo = comboRonin(axies);
+	/* const roninCombo = comboRonin(axies);
+	const _50usd = lessThan(axies, 110);
 	const _fishKoiBreed0 = fishKoiBreed0(axies);
+	const _BirdDamage = birdDamage(axies);
+	const _BirdDamagePigeon = birdDamagePigeon(axies);
+	const _comboDualBlade = comboDualBlade(axies);
 	const _fishKoi = fishKoi(axies);
 	const _fishNimo = fishnimo(axies);
-	const _fishAnemone = fishAnemone(axies);
+	const _fishAnemone = fishAnemone(axies); */
 	const _PlantStandar = plantStandard(axies);
-	const final = roninCombo?.concat(
+	const _plantStandardBreed0 = plantStandardBreed0(axies);
+	const _plantStandardBug = plantStandardBug(axies);
+	/* 	const _terminator = terminator(axies); */
+	const final = _PlantStandar?.concat(
+		_PlantStandar /* 
 		_50usd,
-		_PlantStandar,
 		_fishKoiBreed0,
 		_fishKoi,
 		_fishAnemone,
-		_fishNimo
+		_fishNimo,
+		_BirdDamage,
+		_BirdDamagePigeon,
+		_terminator,
+		_comboDualBlade, */,
+		_plantStandardBreed0,
+		_plantStandardBug
 	);
 
 	useEffect(() => {
@@ -53,7 +71,7 @@ const RecentlyListed = ({getEthPrice, getAxieLatest, filters}) => {
 		timerUpdateAxies();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [getAxieLatest, getEthPrice]);
-
+	console.log(axies);
 	return (
 		<div className='content'>
 			<StyledContainer>
@@ -64,12 +82,27 @@ const RecentlyListed = ({getEthPrice, getAxieLatest, filters}) => {
 				<div className='filters'>
 					<Filters />
 				</div>
-
+				{final &&
+					final[0] &&
+					window.open(
+						`https://marketplace.axieinfinity.com/axie/${final[0]?.id}`,
+						'_blank',
+						'status=yes,scrollbars=yes,toolbar=yes,resizable=yes,width=1200,height=1000,menubar'
+					)}
 				<div>
 					<div className='dogs'>
 						<div className='div_cards'>
 							{final ? (
 								final.map((el, i) => <SaleCard key={i} axie={el} ETH={ETH} />)
+							) : (
+								<Spinner />
+							)}
+						</div>
+					</div>
+					<div className='dogs'>
+						<div className='div_cards'>
+							{_250usd ? (
+								_250usd.map((el, i) => <SaleCard key={i} axie={el} ETH={ETH} />)
 							) : (
 								<Spinner />
 							)}
--- src/utils/filterVars.js
+++ src/utils/filterVars.js
@@ -1,20 +1,8 @@
-const result = (parts, classs) => {
-	console.log('object,', classs);
-	const control = parts?.reduce((acum, part) => {
-		if (part?.class === classs) {
-			acum++;
-		}
-		return acum;
-	}, 0);
-	return control;
-};
-
 export const fishKoiBreed0 = (axies) => {
-	console.log(result);
 	if (axies.length)
 		return axies.filter(
 			({parts, auction, breedCount, stats, class: classs}) =>
-				auction?.currentPriceUSD < 450 &&
+				auction?.currentPriceUSD < 370 &&
 				parts[2]?.id === 'back-goldfish' &&
 				parts[3]?.id === 'mouth-risky-fish' &&
 				parts[4]?.id === 'horn-shoal-star' &&
@@ -24,39 +12,11 @@ export const fishKoiBreed0 = (axies) => {
 				stats.speed === 57
 		);
 };
-export const plantStandard = (axies) => {
-	if (axies.length)
-		return axies.filter(
-			({parts, auction, breedCount, stats, class: classs}) =>
-				auction?.currentPriceUSD < 350 &&
-				parts[2]?.id === 'back-pumpkin' &&
-				parts[3]?.id === 'mouth-serious' &&
-				parts[4]?.id === 'horn-cactus' &&
-				parts[5]?.id === 'tail-carrot' &&
-				breedCount === 0 &&
-				classs === 'Plant'
-		);
-};
-
-export const comboRonin = (axies) => {
-	if (axies.length)
-		return axies.filter(
-			({parts, auction, stats, class: classs}) =>
-				auction?.currentPriceUSD < 320 &&
-				parts[2]?.id === 'back-ronin' &&
-				parts[3]?.id === 'mouth-nut-cracker' &&
-				parts[4]?.id === 'horn-imp' &&
-				parts[5]?.id === 'tail-nut-cracker' &&
-				classs === 'Beast' &&
-				stats.speed >= 40
-		);
-};
-
 export const fishKoi = (axies) => {
 	if (axies.length)
 		return axies.filter(
 			({parts, auction, stats, class: classs}) =>
-				auction?.currentPriceUSD < 390 &&
+				auction?.currentPriceUSD < 345 &&
 				parts[2]?.id === 'back-goldfish' &&
 				parts[3]?.id === 'mouth-risky-fish' &&
 				parts[4]?.id === 'horn-shoal-star' &&
@@ -69,7 +29,7 @@ export const fishnimo = (axies) => {
 	if (axies.length)
 		return axies.filter(
 			({parts, auction, stats, class: classs}) =>
-				auction?.currentPriceUSD < 390 &&
+				auction?.currentPriceUSD < 355 &&
 				parts[2]?.id === 'back-goldfish' &&
 				parts[3]?.id === 'mouth-risky-fish' &&
 				parts[4]?.id === 'horn-shoal-star' &&
@@ -82,13 +42,134 @@ export const fishAnemone = (axies) => {
 	if (axies.length)
 		return axies.filter(
 			({parts, auction, stats, class: classs}) =>
-				auction?.currentPriceUSD < 610 &&
+				auction?.currentPriceUSD < 448 &&
 				parts[2]?.id === 'back-anemone' &&
 				parts[3]?.id === 'mouth-risky-fish' &&
 				parts[4]?.id === 'horn-anemone' &&
 				parts[5]?.id === 'tail-nimo' &&
 				classs === 'Aquatic' &&
 				stats.speed >= 57
+		);
+};
+export const plantStandard = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, breedCount, stats, class: classs}) =>
+				auction?.currentPriceUSD < 360 &&
+				parts[2]?.id === 'back-pumpkin' &&
+				parts[3]?.id === 'mouth-serious' &&
+				parts[4]?.id === 'horn-cactus' &&
+				parts[5]?.id === 'tail-carrot' &&
+				classs === 'Plant' &&
+				stats.hp === 61
+		);
+};
+export const plantStandardBug = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, breedCount, stats, class: classs}) =>
+				auction?.currentPriceUSD < 428 &&
+				parts[2]?.id === 'back-pumpkin' &&
+				parts[3]?.id === 'mouth-serious' &&
+				parts[4]?.id === 'horn-leaf-bug' &&
+				parts[5]?.id === 'tail-carrot' &&
+				classs === 'Plant' &&
+				stats.hp > 55
+		);
+};
+export const plantStandardBreed0 = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, breedCount, stats, class: classs}) =>
+				auction?.currentPriceUSD < 383 &&
+				parts[2]?.id === 'back-pumpkin' &&
+				parts[3]?.id === 'mouth-serious' &&
+				parts[4]?.id === 'horn-cactus' &&
+				parts[5]?.id === 'tail-carrot' &&
+				breedCount === 0 &&
+				classs === 'Plant' &&
+				stats.hp === 61
+		);
+};
+export const terminator = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, breedCount, stats, class: classs}) =>
+				auction?.currentPriceUSD < 450 &&
+				parts[2]?.id === 'back-snail-shell' &&
+				parts[3]?.id === 'mouth-tiny-turtle' &&
+				parts[4]?.id === 'horn-lagging' &&
+				parts[5]?.id === 'tail-thorny-caterpillar' &&
+				classs === 'Reptile'
+		);
+};
+export const birdDamage = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, breedCount, stats, class: classs}) =>
+				auction?.currentPriceUSD < 448 &&
+				parts[2]?.id === 'back-kingfisher' &&
+				parts[3]?.id === 'mouth-little-owl' &&
+				parts[4]?.id === 'horn-kestrel' &&
+				parts[5]?.id === 'tail-the-last-one' &&
+				classs === 'Bird' &&
+				stats.hp === 27 &&
+				stats.speed === 61
+		);
+};
+export const birdDamagePigeon = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, breedCount, stats, class: classs}) =>
+				auction?.currentPriceUSD < 447 &&
+				parts[2]?.id === 'back-pigeon-post' &&
+				parts[3]?.id === 'mouth-little-owl' &&
+				parts[4]?.id === 'horn-kestrel' &&
+				parts[5]?.id === 'tail-the-last-one' &&
+				classs === 'Bird' &&
+				stats.hp === 27 &&
+				stats.speed === 61
+		);
+};
+/* export const birdDamageBreed0 = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, breedCount, stats, class: classs}) =>
+				auction?.currentPriceUSD < 448 &&
+				parts[2]?.id === 'back-kingfisher' &&
+				parts[3]?.id === 'mouth-little-owl' &&
+				parts[4]?.id === 'horn-kestrel' &&
+				parts[5]?.id === 'tail-the-last-one' &&
+				classs === 'Bird' &&
+				stats.hp === 27 &&
+				stats.speed === 61
+		);
+}; */
+
+export const comboRonin = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, stats, class: classs}) =>
+				auction?.currentPriceUSD < 325 &&
+				parts[2]?.id === 'back-ronin' &&
+				parts[3]?.id === 'mouth-nut-cracker' &&
+				parts[4]?.id === 'horn-imp' &&
+				parts[5]?.id === 'tail-nut-cracker' &&
+				classs === 'Beast' &&
+				stats.speed >= 41
+		);
+};
+export const comboDualBlade = (axies) => {
+	if (axies.length)
+		return axies.filter(
+			({parts, auction, stats, class: classs}) =>
+				auction?.currentPriceUSD < 340 &&
+				parts[2]?.id === 'back-ronin' &&
+				parts[3]?.id === 'mouth-nut-cracker' &&
+				parts[4]?.id === 'horn-dual-blade' &&
+				parts[5]?.id === 'tail-nut-cracker' &&
+				classs === 'Beast' &&
+				stats.speed >= 41
 		);
 };
 
